a21a102579e1f6b9edfec269d392cab24216c29c
cyx-code <1741914950@qq.com>
2020-05-22 21:09:04 +0800

    先序、中序遍历，生成二叉树

---
+++ src/main/java/cyx/solution/leetcode/tree/BuildTree.java
@@ -0,0 +1,23 @@
+package cyx.solution.leetcode.tree;
+
+import cyx.solution.leetcode.common.TreeNode;
+
+import java.util.Arrays;
+
+public class BuildTree {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length < 1) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        for (int i = 0; i < preorder.length; i++) {
+            if (preorder[0] == inorder[i]) {
+                root.left = buildTree( Arrays.copyOfRange(preorder, 1, i + 1), Arrays.copyOfRange(inorder, 0, i));
+                root.right = buildTree( Arrays.copyOfRange(preorder, i + 1, preorder.length), Arrays.copyOfRange(inorder, i + 1, inorder.length));
+
+            }
+        }
+        return root;
+    }
+
+}
