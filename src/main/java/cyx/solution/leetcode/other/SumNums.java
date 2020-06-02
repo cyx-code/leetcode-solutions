583ccd14ec5ed3e48ed88d6a0ea261cd2a8599dd
cyx-code <1741914950@qq.com>
2020-06-02 22:02:11 +0800

    求1+2+…+n （递归解法）

--- src/main/java/cyx/solution/leetcode/other/SumNums.java
+++ src/main/java/cyx/solution/leetcode/other/SumNums.java
@@ -5,7 +5,9 @@ package cyx.solution.leetcode.other;
  */
 public class SumNums {
     public int sumNums(int n) {
-        return (int) (Math.pow(n, 2) + n) >> 1;
+        // return (int) (Math.pow(n, 2) + n) >> 1;
+        boolean flag = n > 0 && (n += sumNums(n - 1)) > 0;
+        return n;
     }
 
     public static void main(String[] args) {
