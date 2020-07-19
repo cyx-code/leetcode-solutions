4cb6f9a201f06f5895a8168551099be1bfe6f4bd
cyx-code <chenyuxiang008@deppon.com>
2020-07-19 23:29:05 +0800

    移动零

---
+++ src/main/java/cyx/solution/leetcode/array/MoveZeroes.java
@@ -0,0 +1,75 @@
+package cyx.solution.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ *
+ * 示例:
+ *
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ *
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/move-zeroes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MoveZeroes {
+    /**
+     * 暴力解法
+     * @param nums
+     * @param
+     * @param
+     */
+    /*public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 0) {
+                int left = i;
+                int right = i + 1;
+                while (right < nums.length) {
+                    if (nums[right] != 0) {
+                        swap(nums, left, right);
+                        left = right;
+                    }
+                    right++;
+                }
+            }
+        }
+    }
+*/
+
+    /**
+     * 采用快速排序的方式，大于0的就放到左边，等于0的右边
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        if(nums==null) {
+            return;
+        }
+        //两个指针i和j
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            //当前元素!=0，就把其交换到左边，等于0的交换到右边
+            if(nums[i] !=0 ) {
+               swap(nums, i, j);
+               j++;
+            }
+        }
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+        new MoveZeroes().moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+
