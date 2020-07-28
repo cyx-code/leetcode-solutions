5003fe05bcdfb774a508254590e344684f24990d
cyx-code <1741914950@qq.com>
2020-07-28 23:20:34 +0800

    二叉树的直径

---
+++ src/main/java/cyx/solution/leetcode/tree/DiameterOfBinaryTree.java
@@ -0,0 +1,19 @@
+package cyx.solution.leetcode.tree;
+
+import cyx.solution.leetcode.common.TreeNode;
+
+public class DiameterOfBinaryTree {
+    int ans = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+        ans = Math.max(ans, l + r);
+        return Math.max(l, r) + 1;
+    }
+}
